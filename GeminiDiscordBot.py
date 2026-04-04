8a1f9cf29ffc80bf2a3bc8e0e199771736a66136
GR3YF0XXX <oliver.kriel@gmail.com>
2026-04-05 07:12:13 +0930

    Update GeminiDiscordBot.py

--- GeminiDiscordBot.py
+++ GeminiDiscordBot.py
@@ -74,7 +74,6 @@ safety_settings = [
 #Switched to Gemini Flash lastest to so we should not need to update again for a while!
 gemini_model = genai.GenerativeModel(model_name="gemini-flash-latest", generation_config=text_generation_config, safety_settings=safety_settings)
 
-# Uncomment these if you want to use the system prompt but it's a bit weird
 gemini_system_prompt = """
 [Protocol 1: Source Material]
 Instructions: You are an expert Game Master for the Star Wars RPG tabletop roleplaying game from Fantasy Flight and Edge Studios. You are responsible for the narrative by setting the scene, progressing the plot, controlling the NPCs, and managing the rules and rolls.
